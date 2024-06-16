248c653f8aaecf987a9a3cb1fd595d2fc581f1f3
nzajk <nzajkeskovic@outlook.com>
2024-06-16 15:00:56 -0400

    Created a popup panel to display the etymology of a highlighted word.

--- extension/content.js
+++ extension/content.js
@@ -1,4 +1,8 @@
 document.addEventListener('mouseup', async function() {
+    if (event.target.closest('#extension-panel')) {
+        return;
+    }
+
     var word = window.getSelection().toString().trim();
 
     if (word) {
@@ -15,12 +19,47 @@ async function getEtymology(word) {
     if (response.ok) {
         return data['etymology'][0];
     } else {
-        throw new Error(e);
+        throw new Error('Error in fetching etymology');
     }
 }
 
-// update the word and etymology within the popup window
+// create and display a panel to show the word and etymology
+function extensionPanel() {
+    var panel = document.createElement('div');
+    panel.id = 'extension-panel';
+    panel.style.position = 'fixed';
+    panel.style.bottom = '15px';
+    panel.style.right = '15px';
+    panel.style.width = '300px';
+    panel.style.padding = '12.5px';
+    panel.style.backgroundColor = 'white';
+    panel.style.border = '2px solid black';
+
+    var wordElement = document.createElement('p');
+    wordElement.id = 'word';
+    panel.appendChild(wordElement);
+
+    var etymologyElement = document.createElement('p');
+    etymologyElement.id = 'etymology';
+    panel.appendChild(etymologyElement);
+
+    var exitButton = document.createElement('button');
+    exitButton.innerHTML = 'exit';
+    exitButton.style.float = 'right';
+    exitButton.onclick = function() {
+        document.body.removeChild(panel);
+    };
+    panel.appendChild(exitButton);
+
+    document.body.appendChild(panel);
+}
+
+// update the word and etymology within the floating panel
 function displayEtymology(word, etymology) {
-    document.getElementById('word').innerHTML = word;
-    document.getElementById('etymology').innerHTML = etymology;
+    if (!document.getElementById('extension-panel')) {
+        extensionPanel();
+    }
+
+    document.getElementById('word').innerHTML = `Word: ${word}`;
+    document.getElementById('etymology').innerHTML = `Etymology: ${etymology}`;
 }
