3b070a5d93f97c363b440f4ab2a4fa15aec8a93c
nzajk <nzajkeskovic@outlook.com>
2024-06-16 11:45:50 -0400

    Enabled CORS to allow front-end requests

--- server/app.py
+++ server/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+CORS(app)
 
 def scrape_etymology(word):
     url = f'https://www.etymonline.com/word/{word}'
