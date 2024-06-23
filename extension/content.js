8af2f9befb6835ec6f33d141f49ad2209fcc362e
nzajk <nzajkeskovic@outlook.com>
2024-06-23 09:03:23 -0400

    Bug fix: Searching within the extension popup

--- extension/content.js
+++ extension/content.js
@@ -1,5 +1,10 @@
-document.addEventListener('mouseup', async function() {
+// this listener includes the main functionality for the web extension
+document.addEventListener('dblclick', async function() {
     if (event.target.closest('#extension-panel')) {
+        return;
+    }
+
+    if (event.target.closest('#popup-tab')) {
         return;
     }
 
@@ -8,7 +13,7 @@ document.addEventListener('mouseup', async function() {
     if (word) {
         var etymology = await getEtymology(word);
         displayEtymology(word, etymology);
-    }
+    };
 });
 
 // the API call for the etymology
@@ -23,17 +28,25 @@ async function getEtymology(word) {
     }
 }
 
-// create and display a panel to show the word and etymology
+// create and display a panel to show the word and etymology (css styling for the extension box)
 function extensionPanel() {
     var panel = document.createElement('div');
     panel.id = 'extension-panel';
     panel.style.position = 'fixed';
     panel.style.bottom = '15px';
     panel.style.right = '15px';
-    panel.style.width = '300px';
-    panel.style.padding = '12.5px';
-    panel.style.backgroundColor = 'white';
+    panel.style.width = '300px'; 
+    panel.style.height = '200px';
+    panel.style.padding = '5px';
+    // panel.style.margin = '5px';
+    panel.style.backgroundColor = '#e9ecef';
+    panel.style.color = 'black';
     panel.style.border = '2px solid black';
+    panel.style.borderColor = '#343a40';
+    panel.style.overflowY = 'scroll';  // make the panel scrollable
+    panel.style.zIndex = '10000'; // make the panel appear on top of everything
+    panel.style.boxSizing = 'border-box';
+    panel.style.borderRadius = '3%';
 
     var wordElement = document.createElement('p');
     wordElement.id = 'word';
@@ -44,8 +57,11 @@ function extensionPanel() {
     panel.appendChild(etymologyElement);
 
     var exitButton = document.createElement('button');
-    exitButton.innerHTML = 'exit';
-    exitButton.style.float = 'right';
+    exitButton.innerHTML = 'x';
+    exitButton.style.position = 'absolute';
+    exitButton.style.top = '5px';
+    exitButton.style.right = '5px';
+    exitButton.style.paddingRight = '7.5px';
     exitButton.onclick = function() {
         document.body.removeChild(panel);
     };
@@ -60,6 +76,8 @@ function displayEtymology(word, etymology) {
         extensionPanel();
     }
 
-    document.getElementById('word').innerHTML = `Word: ${word}`;
-    document.getElementById('etymology').innerHTML = `Etymology: ${etymology}`;
+    word = word.charAt(0).toUpperCase() + word.slice(1); // capitalize the first letter of the word
+
+    document.getElementById('word').innerHTML = `<strong>Word:</strong> ${word}`;
+    document.getElementById('etymology').innerHTML = `<strong>Etymology:</strong> ${etymology}`;
 }
